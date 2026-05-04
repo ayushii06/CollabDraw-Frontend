f3503166bc7f63a303b23bd5454cd75020939ed0
ayushi <ayushipal06@gmail.com>
2026-05-04 15:32:55 +0530

    Socket URL updated

--- app/socket/socketClient.ts
+++ app/socket/socketClient.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const URL = "https://collab-draw-backend.vercel.app";
+const URL = "https://collabdraw-backend-40qt.onrender.com";
 
 export const socket: Socket = io(URL, {
   autoConnect: false,
